cbba3266c9f2e903aa8ce969857aa2dff35caf36
John Mathews <95311710+thejohnmathews@users.noreply.github.com>
2023-04-24 16:43:06 -0400

    2,2 done

--- sde2.ml
+++ sde2.ml
@@ -1,13 +1,24 @@
 (* SDE 2 CPSC 3520 John Mathews*)
 
 (* 2.1 first_duplicate - first integer in list with first duplicate*)
-let rec first_duplicate numlist =
+let rec first_duplicate numlist = 
   match numlist with
-  (* base case: empty list return -10000*)
+  (* base case: empty list return -10000 *)
   | [] -> -10000 
   (* search for duplicate, take 'value' from list then compare to 'rest' of the numbers*)
   | value :: rest ->
     if List.mem value rest then value
-    else first_duplicate rest
+    else first_duplicate rest;;
 
 (*2.2 first_nonrepeating - first item that is not in the list more than once*)
+let rec helper_nonrepeating = function
+    ([], numlist) -> -10000
+  | (value::rest, numlist) ->
+    if not(List.mem value rest || List.mem value numlist) then value
+    else helper_nonrepeating(rest, value::numlist);;
+
+let rec first_nonrepeating = function
+    (numlist) ->
+      helper_nonrepeating(numlist, []);;
+
+(*2.3 sumOfTwo - return boolean if a+b (!)= v*)
