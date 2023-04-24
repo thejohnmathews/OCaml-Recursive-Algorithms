6f35ce48a5c2318937568df2fe625f43cb250798
John Mathews <95311710+thejohnmathews@users.noreply.github.com>
2023-04-24 15:48:20 -0400

    2.1 done

--- sde2.ml
+++ sde2.ml
@@ -1,13 +1,13 @@
 (* SDE 2 CPSC 3520 John Mathews*)
 
 (* 2.1 first_duplicate - first integer in list with first duplicate*)
-let rec first_duplicate lst =
-  match lst with
-  | [] -> -1000 (* Base case: empty list, return 0 *)
-  | x :: xs ->
-    (* Check if x appears again in the list *)
-    if List.mem x xs then x
-    (* Otherwise, recurse on the remaining list *)
-    else first_duplicate xs
+let rec first_duplicate numlist =
+  match numlist with
+  (* base case: empty list return -10000*)
+  | [] -> -10000 
+  (* search for duplicate, take 'value' from list then compare to 'rest' of the numbers*)
+  | value :: rest ->
+    if List.mem value rest then value
+    else first_duplicate rest
 
-val first_duplicate : int List -> int = <fun>
+(*2.2 first_nonrepeating - first item that is not in the list more than once*)
